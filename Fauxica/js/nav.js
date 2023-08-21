29fe9540282735fe729af16f02bd6dc03f2ac37b
dattebayo7 <125813724+dattebayo7@users.noreply.github.com>
2023-08-21 12:40:12 +0530

    Update nav.js

--- Fauxica/js/nav.js
+++ Fauxica/js/nav.js
@@ -13,7 +13,7 @@ const createNav = () => {
     nav.innerHTML=`
     <a href="#"><img src="/images/images/logo.svg" alt="xyz"></a>
                 <ul> 
-                    <li class="nav_item"><a class="nav_link" href="http://127.0.0.1:5501/html/index.html">Home</a></li>
+                    <li class="nav_item"><a class="nav_link" href="http://127.0.0.1:5501/index.html">Home</a></li>
                     <li class="nav_item"><a class="nav_link" href="http://127.0.0.1:5501/html/products1.html">Product</a></li>
                     <li class="nav_item"><a class="nav_link" href="http://127.0.0.1:5501/html/cart.html">Cart</a></li>
                     <li class="nav_item"><a class="nav_link" href="http://127.0.0.1:5501/html/login.html?#">Login</a></li>
