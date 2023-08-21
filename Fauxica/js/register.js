08f3aaafc4e5f9997d2428cf07ce2235f2bea5a5
dattebayo7 <125813724+dattebayo7@users.noreply.github.com>
2023-08-21 16:11:21 +0530

    Delete Fauxica directory

--- Fauxica/js/register.js
+++
@@ -1,7 +0,0 @@
-let btnclick=document.querySelector('button')
-btnclick.addEventListener("click",(e)=>{
-    if(document.getElementById('input-text').value!==''){
-         let url="http://127.0.0.1:5501/html/login.html"
-         window.open(url,"_self")
-    }
-})
